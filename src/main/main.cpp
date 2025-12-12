751373df0c40f246bd3f9584503a4bcfda4ad948
FeintWitch <sniffleslou@gmail.com>
2025-12-12 16:26:59 +0000

    window set up done
    basically it runs cool!
    
    my next goal is to actually start yippee

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -2,28 +2,58 @@
 //
 
 #include <iostream>
-#include "SDL2/SDL.h"
+
 #include "GL/glew.h"
 #include <GLFW/glfw3.h>
+#include "main.h"
 using namespace std;
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
-//Initialise glfw and the window as a objecy 
+    //Initialise glfw and the window as a objecy 
     glfwInit();
     GLFWwindow* window = glfwCreateWindow(1280, 720, "silver", NULL, NULL);
     //success?
-    if (window = NULL) {
+    if (window == NULL) {
         cout << "GLFW window did not instantiate\n";
         glfwTerminate();
         return -1;
     }
     glfwMakeContextCurrent(window);
     glewInit();
-    glViewport(0,0,1280,720);
-    
+    glViewport(0, 0, 1280, 720);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    float verticles[] = {
+        -0.5f,-0.5f,0.0f, //pos 0. x,y,z
+        0.5f,-0.5f,0.0f,//pos
+        0.0f,0.0f,0.0f//pos 2
+    };
+    //now the render loop
+    while (glfwWindowShouldClose(window) == false)
+    {
+        ProcessUserInput(window); //takes userinput
+        //renders
+        glClearColor(0.25f, 0.0f, 1.0f, 1.0f);// COLOUR TO DISPLAY
+        glClear(GL_COLOR_BUFFER_BIT);//CLEARS THE COLOUR
+        //refreshes
+        glfwSwapBuffers(window);//swaps buffer colour
+        glfwPollEvents(); 
+    }
+
 
     return 0;
+}
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    //resizes window besded on values
+    glViewport(0, 0, width, height);
+}
+void ProcessUserInput(GLFWwindow* WindowIn) {
+    //close when exist is pressed
+    if (glfwGetKey(WindowIn, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(WindowIn, true);
+    }
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
--- src/main/main.h
+++ src/main/main.h
@@ -1,5 +1,9 @@
 #pragma once
 
-#include <gk>
+#include <GLFW/glfw3.h>
 
+//called to resize winow
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+//user input proccesses on aw window
+void ProcessUserInput(GLFWwindow* WindowIn);
 
